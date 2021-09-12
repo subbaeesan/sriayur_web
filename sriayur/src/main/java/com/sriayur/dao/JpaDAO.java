8f636f7b104ef4d5d517c17b1daa3c4dfb5ace40
subbaeesan <subbaeesan@gmail.com>
2021-09-12 23:45:53 +0800

    update to create DAO

---
+++ sriayur/src/main/java/com/sriayur/dao/JpaDAO.java
@@ -0,0 +1,30 @@
+package com.sriayur.dao;
+
+import javax.persistence.EntityManager;
+
+public class JpaDAO<T> {
+	
+	protected EntityManager entityManager;
+
+	public JpaDAO(EntityManager entityManager) {
+		super();
+		this.entityManager = entityManager;
+	}
+	
+	
+	public T create(T t) {
+		
+		entityManager.getTransaction().begin();
+		
+		entityManager.persist(t);
+		entityManager.flush();
+		entityManager.refresh(t);
+			
+		
+		entityManager.getTransaction().commit();
+		
+		return t;
+		
+	}
+
+}
