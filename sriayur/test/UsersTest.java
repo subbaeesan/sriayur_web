48c528e198957dbba43928cf647582e3a8c0842d
subbaeesan <subbaeesan@gmail.com>
2021-09-13 01:21:38 +0800

    update DAO for JUnit and successfully tested

--- sriayur/test/UsersTest.java
+++ sriayur/test/UsersTest.java
@@ -1,37 +1,31 @@
-package com.sriayur.dao;
-
-import static org.junit.Assert.*;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.junit.Test;
-
 import com.sriayur.entity.Users;
 
-public class UserDAOTest {
+public class UsersTest {
 
-	@Test
-	public void testCreateUsers() {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		Users user1 = new Users();
 		user1.setEmail("subbaeesan@hotmail.com");
 		user1.setFullName("Subbaeesan");
 		user1.setPassword("subbaeesan");
 		
-	
+		
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("sriayur");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
-		UserDAO userDAO = new UserDAO(entityManager);
-		user1 = userDAO.create(user1);
+		entityManager.getTransaction().begin();
+		entityManager.persist(user1);
 		
-		
-		
-		
+		entityManager.getTransaction().commit();
 		entityManager.close();
 		entityManagerFactory.close();
 		
+		
+		System.out.println("A users object was persisted");
 		
 		
 	}
